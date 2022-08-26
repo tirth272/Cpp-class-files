bc0809d43276d18516bc9860efaf751437da9fd2
tirth272 <shahtirth24680@gmail.com>
2022-08-26 17:24:13 +0530

    ok

--- 26-8/Virtual_Class.cpp
+++ 26-8/Virtual_Class.cpp
@@ -25,3 +25,15 @@ int main()
     d.msg();
     return 0;
 }
+/*
+    DDOD: Deadly diamond of death : Ambiguous Error
+
+                                    Virtual_Class.cpp: In function 'int main()':
+                                Virtual_Class.cpp:25:7: error: request for member 'msg' is ambiguous
+                                     d.msg();
+                                       ^~~
+                                Virtual_Class.cpp:7:10: note: candidates are: void A::msg()
+                                     void msg()
+                                          ^~~
+                                Virtual_Class.cpp:7:10: note:                 void A::msg()
+*/
